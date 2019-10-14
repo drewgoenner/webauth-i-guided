a5d0f3e9942584c13d0a7acd5d9c7edb07abba82
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-10-14 12:56:09 -0500

    started working on middleware

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+  // validate the user
+
+  // hash the password
+  const hash = bcrypt.hashSync(user.password, 14);
+
+  // we override the password with the hash
+
+  user.password = hash;
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -31,21 +40,29 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
-  Users.findBy({ username })
+  if (username && password) {
+    Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: 'You cannot pass!' });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
+
+  } else {
+    res.status(400).json({ message: 'please provide credentials'})
+  }
+
+  
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -56,15 +73,37 @@ server.get('/api/users', (req, res) => {
 server.get('/hash', (req, res) => {
   //read a password from the Authorization header
   //return an object with the password hashed using bcryptjs
-  const creds = req.body
-  const hash = bcrypt.hashSync(creds.password, 14);
-  creds.password = hash
+  const pass = req.headers.authorization;
+
+  if(pass) {
+  // that 14 is how we slow down attackers trying to pre-generate hashes
+  const hash = bcrypt.hashSync(pass, 14); //the 14 is the number of rounds 2 ^ 14
+  // a good starting value is 14
+  
   // bcrypt.compareSync("$tuff", hash)
 
-  console.log(hash)
+  res.status(200).json({ hash })
+} else {
+  res.status(400).json({ message: 'please provide credentials'});
+}
+
+
   
 
 })
+
+// implemented protected middleware that will check for username and password
+// in the headers and if valid provide access to the endpoints
+function protected(res, req, next) {
+  const { username, password } = req.headers;
+
+  if (username && bcrypt.compareSync(password, user.password)) {
+   next();
+  } else {
+    res.status(400).json({ message: 'no creds'})
+  }
+
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
